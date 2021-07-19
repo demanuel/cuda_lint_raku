8797d2715320363fa784dc765d9bcade1a69c453
demanuel <demanuel@ymail.com>
2021-07-19 23:24:29 +0200

    Linter is now more generic so it can support more different message types
    It searches for the separator between message and location.
    For example, it now looks for the ':' of the error message. If it doesn't match it looks for 'at'.

--- linter.py
+++ linter.py
@@ -1,10 +1,9 @@
 import re
 from cuda_lint import Linter, util
 
-
 class RakuLint(Linter):
-
     """Provides an interface to raku -c"""
+
     cmd = None
     executable = 'raku'
     multiline = True
@@ -12,27 +11,35 @@ class RakuLint(Linter):
     base_cmd = ('-c')
     tempfile_suffix = '.raku'
 
-    
     def __init__(self, lint):
-        self.errors=[]
-        self.regexes = {'generic' : r'((?:Variable|Unsupported|Missing|Type).*)(.*\s)+(?:(?:at .*\:(\d+)))', 'multiple' : r'(.*?)s:'}
+        self.reason_regexes = [re.compile(r'(.*):', re.DOTALL|re.MULTILINE), re.compile(r'(.*)\vat',re.DOTALL|re.MULTILINE)]
+        self.plural_regex   = re.compile(r'(.*)s:')
+        self.line_regexes   = [re.compile(r':(\d+)'), re.compile('\s(\d+),?')]
+
         super().__init__(lint)
-    
-
-    def find_errors(self, output):
-        body = '\n'.join(output.split('\n')[1:])
-        match = re.match(self.regexes['multiple'], body)
-
-        if match:
-            regex = r'line (\d+)';
-            for line in re.findall(regex, body):
-                yield True, int(line), 0, '','', match.group(1),''
-        else:
-            for result in re.findall(self.regexes['generic'], body):
-                yield True, int(result[-1]), 0, '', '', result[0] if len(result) == 2 else ' '.join(result[:-1]), ''
 
     def cmd(self):
         """Return the command line to execute."""
+
         result = self.executable + ' ' + self.base_cmd
 
         return result
+
+    def find_errors(self, output):
+        body = '\n'.join(output.split('\n')[1:])
+        print(body)
+
+        for reason in self.reason_regexes:
+            m = reason.match(body)
+            if m:
+                reason = m.group(1)
+                body = body.replace(reason, '') # remove the reason
+                plural = self.plural_regex.match(reason)
+
+                if plural:
+                    reason = plural.group(1)
+
+                for lrg in self.line_regexes:
+                    lines = lrg.findall(body)
+                    for line in lines:
+                        yield True, int(line), 0, '', '', reason,''
